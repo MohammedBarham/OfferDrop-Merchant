bb337321f0b55531742695450d40e08afff535f2
Mohammed Barham <mohammed.barham@exalt.ps>
2013-12-24 15:22:45 +0200

    removing the proj

--- OfferDrop-Merchant/ODFrames.h
+++
@@ -1,9 +0,0 @@
-//
-//  ODFrames.h
-//  OfferDrop-Merchant
-//
-//  Created by Mohammed Barham on 11/6/13.
-//  Copyright (c) 2013 OfferDrop. All rights reserved.
-//
-
-#define RIGHT_MENU_ITEM_TRANSMIT_VIEW_FRAME CGRectMake(0.0, 0.0, 210.0, 40.0)
